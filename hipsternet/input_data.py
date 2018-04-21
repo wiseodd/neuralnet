0eddce75e4457150bd4a2a8cd301fc17ca12ea7e
gitjags <jag@singh.haus>
2018-04-21 14:33:32 -0700

    Python 3.5.1 needs urllib.request module for urlretrieve. The python vesion is forced via conda in the environment.yaml

--- hipsternet/input_data.py
+++ hipsternet/input_data.py
@@ -1,7 +1,7 @@
 """Functions for downloading and reading MNIST data."""
 import gzip
 import os
-import urllib
+import urllib.request
 import numpy
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
 
@@ -12,7 +12,7 @@ def maybe_download(filename, work_directory):
         os.mkdir(work_directory)
     filepath = os.path.join(work_directory, filename)
     if not os.path.exists(filepath):
-        filepath, _ = urllib.urlretrieve(SOURCE_URL + filename, filepath)
+        filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
         statinfo = os.stat(filepath)
         print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
     return filepath
